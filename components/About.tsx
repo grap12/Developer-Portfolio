3bb7836ed8fd9b8963de24219ff4788a80f77569
Omar <omarechchenygry@gmail.com>
2025-09-13 21:34:05 +0100

    DONE

--- components/About.tsx
+++ components/About.tsx
@@ -4,32 +4,31 @@ const About: React.FC<{}> = () => {
   return (
     <section
       id="about"
-      className="flex flex-col items-center justify-center h-full relative overflow-hidden pt-[35px] "
-      style={{ transform: "scale(0.9" }}
+      className="flex flex-col items-center justify-center h-full relative overflow-hidden pt-[35px]"
+      style={{ transform: "scale(0.9)" }}
     >
-      <div className="flex flex-col  justify-around flex-wrap items-center   max-w-[900px]">
+      <div className="flex flex-col justify-around flex-wrap items-center max-w-[900px]">
         <h1 className="text-white font-semibold text-6xl">ABOUT ME</h1>
-        <p className=" tracking-[0.5em] text-transparent font-light pb-5  bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500  text-1xl">
+        <p className="tracking-[0.5em] text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl">
           EXPLORE NOW
         </p>
-        <p className="text-gray-300 text-center ">
-  As a passionate freelance software engineer, I thrive on the intricate
-  dance between logic and creativity. My expertise centers around React Native,
-  where I seamlessly blend technology with innovation.
-  <br />
-  <br />
-  With a fervor for crafting elegant solutions, I navigate the
-  ever-evolving landscape of software development. My journey involves
-  translating concepts into code, creating seamless user experiences,
-  and constantly pushing the boundaries of what's possible.
-  <br />
-  <br />
-  Beyond development, I specialize in web application security, ensuring
-  robust and resilient systems. I am also experienced in containerization
-  and orchestration, leveraging Docker and Kubernetes to build scalable,
-  secure, and efficient infrastructures.
-</p>
-
+        <p className="text-gray-300 text-center">
+          As a passionate freelance software engineer, I thrive on the intricate
+          dance between logic and creativity. My expertise centers around React Native,
+          where I seamlessly blend technology with innovation.
+          <br />
+          <br />
+          With a fervor for crafting elegant solutions, I navigate the
+          ever-evolving landscape of software development. My journey involves
+          translating concepts into code, creating seamless user experiences,
+          and constantly pushing the boundaries of what&apos;s possible.
+          <br />
+          <br />
+          Beyond development, I specialize in web application security, ensuring
+          robust and resilient systems. I am also experienced in containerization
+          and orchestration, leveraging Docker and Kubernetes to build scalable,
+          secure, and efficient infrastructures.
+        </p>
       </div>
     </section>
   );
